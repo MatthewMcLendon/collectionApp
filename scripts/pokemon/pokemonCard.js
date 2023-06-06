64e5ebc95b0800877fdd9089636f60b195b7db8b
jwgilliam <ggsmoustache@gmail.com>
2023-06-06 18:48:24 -0500

    working on merging

---
+++ scripts/pokemon/pokemonCard.js
@@ -0,0 +1,17 @@
+const pokemonComponent = (pokemon) => {
+  return `
+    <section class="pokemon_card">
+    <div class="pokemon_header">
+    <div class="pokemon_name">${pokemon.name}</div>
+    <div class="pokemon_picture">
+    <img class="pokemon_art">${pokemon.images.large}</img
+    </div>
+    <div class="pokemon_type">${pokemon.types}</div>
+    <div class="pokemon_price">${pokemon.prices.map()}</div>
+    </div>
+    
+    </section>
+  `
+}
+
+export default pokemonComponent
