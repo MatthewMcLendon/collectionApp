39ee55374a2630d8434318f54206f16b695ee41f
jwgilliam <ggsmoustache@gmail.com>
2023-06-08 13:48:02 -0500

    func: built search form component

--- scripts/main.js
+++ scripts/main.js
@@ -1,6 +1,7 @@
 import { navBar } from "./navigation/navbar.js";
 import { boardgameEventHandler } from "./boardgames/boardgameHandler.js";
 import { latestBoardgame } from "./boardgames/boardgameLatest.js";
+import pokemonSearchComponent from "./pokemon/pokemonCardSearch.js";
 
 navBar();
 
@@ -11,3 +12,5 @@ if (window.location.href === "http://localhost:3000/boardgame") {
 if (window.location.href === "http://localhost:3000/") {
   latestBoardgame();
 }
+
+pokemonSearchComponent()
--- scripts/pokemon/pokemonCardSearch.js
+++ scripts/pokemon/pokemonCardSearch.js
@@ -1,10 +1,39 @@
-const targetElement = document.querySelector(".pokemon-search-container")
+import { getPokemon } from "./pokemonCardProvider.js"
 
-export const pokemonSearchComponent = () => {
-  targetElement.innerHTML = `
-  <form class="pokemon-search-form">
-    <input class="pokemon-search-input" type="text" name="pokemon-search-input"> 
-    <button class="pokemon-search-button" id="pokemon-search-button">Search</button>
-  </form>
-  `
+const eventHub = document.querySelector(".container")
+const contentTarget = document.querySelector(".pokemon-search-container")
+let searchValue = ""
+
+const pokemonSearchComponent = () => {
+  eventHub.addEventListener("click", clickEvent => {
+    if (clickEvent.target.id === "pokemonSearch") {
+      searchValue = document.querySelector("#searchField").value
+      const message = new CustomEvent("pokemonSearchClicked", {
+        detail: {
+          pokemonSearch: searchValue
+        }
+      })
+      eventHub.dispatchEvent(message)
+      clickEvent.preventDefault()
+
+
+    }
+  })
+
+  const render = () => {
+    contentTarget.innerHTML = `
+      <form class="searchForm">
+        <input type="text" id="searchField" isRequired/>
+        <button id="pokemonSearch">search</button>
+      </form>
+    `
+  }
+  render()
 }
+
+export default pokemonSearchComponent
+
+
+
+
+
