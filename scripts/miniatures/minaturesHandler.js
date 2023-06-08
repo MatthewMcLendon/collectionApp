00a2cb45d2a9c34c21c3fa5327cb2bb247c5e500
Matthew McLendon <matthewmclendon1006@gmail.com>
2023-06-08 15:57:56 -0500

    func: miniature form for adding to collection

--- scripts/miniatures/minaturesHandler.js
+++ scripts/miniatures/minaturesHandler.js
@@ -1,6 +1,7 @@
 import { miniatureList } from "./miniaturesList.js";
+import { miniatureForm } from "./miniaturesForm.js";
 import { getMiniatureCollection } from "./miniaturesProvider.js";
 
 export const miniatureEventHandler = () => {
-  getMiniatureCollection().then(miniatureList);
+  getMiniatureCollection().then(miniatureList).then(miniatureForm);
 };
---
+++ scripts/miniatures/miniaturesForm.js
@@ -0,0 +1,30 @@
+const targetElement = document.querySelector(".miniatures-form-container");
+
+export const miniatureForm = () => {
+  targetElement.innerHTML = `
+    <form class="miniature-form">
+        <h2>Add to collection</h2>
+        <div class="miniature-form-divider">
+            <label for="miniature-form-title">Name of miniature:</label>
+            <input type="text" id="miniature-form-title" placeholder="Miniature name">
+        </div>
+        <div class="miniature-form-divider">
+            <label for="miniature-form-game">Game miniature is for:</label>
+            <input type="text" id="miniature-form-game" placeholder="Game for the miniature">
+        </div>
+        <div class="miniature-form-divider">
+            <label for="miniature-form-imageURL">Link to miniature image:</label>
+            <input type="text" id="miniature-form-imageURL" placeholder="URL for image">
+        </div>
+        <div class="miniature-form-divider">
+            <label for="miniature-form-storeURL">Link to store page:</label>
+            <input type="text" id="miniature-form-storeURL" placeholder="URL for store page">
+        </div>
+        <div class="miniature-form-divider">
+            <label for="miniature-form-count">Number in collection:</label>
+            <input type="number" id="miniature-form-count" placeholder="Number in collection">
+     </div>
+     <button id="miniature-form-submit">Submit</button>
+    </form>
+    `;
+};
